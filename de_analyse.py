a2bdc79890e0289ae889899122ad338deca01465
Bas van Stein <steinbvan@liacs.leidenuniv.nl>
2023-11-01 08:44:25 +0000

    analyse de script fixes

--- de_analyse.py
+++ de_analyse.py
@@ -10,8 +10,9 @@ from ConfigSpace import ConfigurationSpace
 from ConfigSpace.util import generate_grid
 from IPython.display import display
 
-data_file = "de_final.pkl"
+data_file = "de_final_processed.pkl"
 df = pd.read_pickle(data_file)
+
 
 features= ['mutation_base', 'mutation_reference', 'CR', "F", 'lambda_',
        'mutation_n_comps', 'use_archive', 'crossover', 'adaptation_method',
@@ -51,10 +52,15 @@ de_explainer = explainer(None,
 
 
 de_explainer.load_results(data_file)
+
+#auc Large
+de_explainer.df.loc[de_explainer.df["dim"] == 30,'auc'] = de_explainer.df.loc[de_explainer.df["dim"] == 30,'aucLarge']
+
+
 #for feature in features:
 #    cmaes_explainer.df[feature] = cmaes_explainer.df[feature].astype("category")
-df = de_explainer.performance_stats()
-display(df.style.bar(cmap='viridis'))
+#df = de_explainer.performance_stats()
+#display(df.style.bar(cmap='viridis'))
 
 #cmaes_explainer.plot(partial_dependence=False, best_config=False)
 #print(de_explainer.stats[5])
