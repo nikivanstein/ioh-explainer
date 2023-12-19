11955c0a85048acb44553ed5dade086f2244ff4a
Bas van Stein <steinbvan@liacs.leidenuniv.nl>
2023-12-19 13:52:21 +0000

    additional coverage

--- tests/test_ioh_xplain.py
+++ tests/test_ioh_xplain.py
@@ -5,7 +5,7 @@ import pytest
 from ConfigSpace import ConfigurationSpace
 from scipy.optimize import differential_evolution
 
-from ioh_xplainer import __version__, explainer
+from ioh_xplainer import __version__, explainer, compare
 
 """
 Configuration space for test functions
@@ -77,6 +77,19 @@ def test_de_run():
     df = test_explainer.performance_stats()
     assert len(df) == 1
 
+    #test explain
+    test_explainer.explain(
+        partial_dependence=True,
+        best_config=True,
+        file_prefix=None,
+        check_bias=False,
+        keep_order=True,
+        catboost_params={
+                "iterations": 10,
+                "depth": 7,
+            },
+    )
+
     # test report
     test_explainer.to_latex_report(
         filename="test_de",
@@ -87,3 +100,46 @@ def test_de_run():
     )
     assert os.path.exists("test_de.tex")
     os.remove("test_de.tex")
+
+def test_analyse_best():
+    test_explainer.run(
+        paralell=False, start_index=0, checkpoint_file="test_paralell.csv"
+    )
+    assert os.path.exists("test_paralell.csv")
+    os.remove("test_paralell.csv")
+    test_explainer.save_results(data_file)
+    assert os.path.exists(data_file)
+    # test loading
+    test_explainer.load_results(data_file)
+    assert "auc" in test_explainer.df.columns
+
+    df = test_explainer.performance_stats()
+    assert len(df) == 1
+
+    # test analyse best
+    test_explainer.analyse_best(filename=None,
+        check_bias=False,
+        full_run=True,
+        full_run_folder="output",
+        reps=1)
+    
+    #test get_results_for_config
+    res = test_explainer.get_results_for_config({"popsize":5, "strategy":"best1bin"}, dim=5, fid=1, iid=1)
+    assert len(res) > 0
+
+    #test get_single_best_for_iid
+    conf, df = test_explainer.get_single_best_for_iid(1,1,5,False)
+    assert len(df) > 0
+
+    conf, df = test_explainer.get_single_best_for_iid(1,1,5,True)
+    assert len(df) > 0
+
+    #test get_average_best
+    conf, df = test_explainer.get_average_best(5,True)
+    assert len(df) > 0
+    conf, df = test_explainer.get_average_best(5,False)
+    assert len(df) > 0
+
+    #test compare
+    df = compare(test_explainer, test_explainer)
+    assert len(df) > 0
